13310aab75d8abcdc4da8491e3ba1529406ed7c3
Benjamin Holmes <ben@revl.world>
2016-02-28 16:44:13 +0000

    First draft with calibration

--- PushYups/app/src/main/java/com/example/bholmes/pushyups/RepActivity.java
+++ PushYups/app/src/main/java/com/example/bholmes/pushyups/RepActivity.java
@@ -10,10 +10,17 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.os.Handler;
+import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.bholmes.pushyups.util.FileIO;
+import java.lang.RuntimeException;
 import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+import com.example.bholmes.pushyups.util.FileIO;
 
 public class RepActivity extends Activity implements SensorEventListener {
     public static final String LOG_TAG = "PushYup";
@@ -22,6 +29,36 @@ public class RepActivity extends Activity implements SensorEventListener {
     private Sensor lightSensor;
 
     private FileOutputStream luxDataFileStream;
+
+    private static final int CALIBRATION_COUNTDOWN = 3;
+    private int countdown = CALIBRATION_COUNTDOWN;
+
+    private static enum State {
+        IDLE ("To begin, place your phone underneath the upper centre of your torso. Then click the button to calibrate the hands free pushup detection for your room."),
+        UP_COUNTDOWN ("Prepare for yourself to be in an upright pushup position in..."),
+        UP_CALIBRATION ("Calibrating UP position, please wait..."),
+        DOWN_COUNTDOWN ("Now prepare yourself to be in a downward pushup position in..."),
+        DOWN_CALIBRATION ("Calibrating DOWN position, please wait..."),
+        COMPLETE ("Calibration completed, you're now ready to begin hands free pushups!"),
+        PUSHUPS ("Pump some iron mofucka!!!");
+
+        private String message;
+
+        State(String message) {
+            this.message = message;
+        }
+    }
+    private State state = State.IDLE;
+
+    private Calibration up_calibration = new Calibration();
+    private Calibration down_calibration = new Calibration();
+
+    TextView status_text = (TextView)findViewById(R.id.status_text);
+    TextView status_message = (TextView)findViewById(R.id.status_message);
+    TextView up_calibration_value = (TextView)findViewById(R.id.up_calibration_value);
+    TextView down_calibration_value = (TextView)findViewById(R.id.down_calibration_value);
+    TextView calibration_countdown_value = (TextView)findViewById(R.id.calibration_countdown_value);
+    Button calibrate_button = (Button)findViewById(R.id.calibrate_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +72,157 @@ public class RepActivity extends Activity implements SensorEventListener {
 
         // Get a file handle for the lux data
         luxDataFileStream = FileIO.getDataFile("luxData.txt");
+
+        // Add handler to the calibration button
+        calibrate_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (state == State.IDLE || state == State.COMPLETE) {
+                    begin_calibration();
+                } else {
+                    cancel_calibration();
+                }
+            }
+        });
     }
+
+    // --------------------------------------------------
+    // Utils
+    // --------------------------------------------------
+
+    private void refresh_display() {
+        // TODO: Set values for up & down calibrations
+        status_text.setText(state.name());
+        up_calibration_value.setText("Average: " + up_calibration.average + " | Max: " + up_calibration.window_max + " | Min: " + up_calibration.window_min);
+        down_calibration_value.setText("Average: " + down_calibration.average + " | Max: " + down_calibration.window_max + " | Min: " + down_calibration.window_min);
+    }
+
+    private class Calibration {
+        public int average = 0;
+        public int window_max = 0;
+        public int window_min = 0;
+        private ArrayList<Integer> light_levels = new ArrayList<Integer>();
+
+        public void calculate() {
+            int total_light = 0;
+            for (int light_level : light_levels) {
+                total_light += light_level;
+            }
+            average = total_light / light_levels.size();
+            window_max = (int)(average * 1.25);
+            window_min = (int)(average * 0.75);
+        }
+
+        public void reset() {
+            average = 0;
+            window_max = 0;
+            window_min = 0;
+            light_levels.clear();
+        }
+    }
+
+    private int generic_countdown(State next_state) {
+        if (countdown == 0) {
+            countdown = CALIBRATION_COUNTDOWN;
+            state = next_state;
+        } else {
+            calibration_countdown_value.setText("" + countdown);
+            countdown -= 1;
+        }
+
+        return 1000;
+    }
+
+    private int calibration_countdown(State next_state, Calibration calibration) {
+        int delay = generic_countdown(State.DOWN_COUNTDOWN);
+
+        if (countdown == CALIBRATION_COUNTDOWN) {
+            // TODO: Begin monitoring light levels
+        } else if (countdown == 0) {
+            // TODO: End monitoring light levels
+
+            // Calculate the final window and update the display
+            calibration.calculate();
+            refresh_display();
+        }
+
+        return delay;
+    }
+
+    // --------------------------------------------------
+    // Calibration state handlers
+    // --------------------------------------------------
+
+    private void begin_calibration() {
+        state = State.UP_COUNTDOWN;
+        up_calibration.reset();
+        down_calibration.reset();
+        refresh_display();
+
+        timerHandler.postDelayed(timerRunnable, 0);
+        calibrate_button.setText("Cancel Calibration");
+    }
+
+    private void cancel_calibration() {
+        timerHandler.removeCallbacks(timerRunnable);
+        state = State.IDLE;
+        calibrate_button.setText("Begin Calibration");
+        calibration_countdown_value.setText("");
+    }
+
+    private int handle_up_countdown() {
+        return generic_countdown(State.UP_CALIBRATION);
+    }
+
+    private int handle_up_calibration() {
+        return calibration_countdown(State.DOWN_COUNTDOWN, up_calibration);
+    }
+
+    private int handle_down_countdown() {
+        return generic_countdown(State.DOWN_CALIBRATION);
+    }
+
+    private int handle_down_calibration() {
+        return calibration_countdown(State.COMPLETE, down_calibration);
+    }
+
+    private int handle_complete() {
+        timerHandler.removeCallbacks(timerRunnable);
+        calibrate_button.setText("Redo Calibration");
+        calibration_countdown_value.setText("");
+        refresh_display();
+        return 0;
+    }
+
+    private int state_factory() {
+        if (state == State.UP_COUNTDOWN) {
+            return handle_up_countdown();
+        } else if (state == State.UP_CALIBRATION) {
+            return handle_up_calibration();
+        } else if (state == State.DOWN_COUNTDOWN) {
+            return handle_down_countdown();
+        } else if (state == State.DOWN_CALIBRATION) {
+            return handle_down_calibration();
+        } else if (state == State.COMPLETE) {
+            return handle_complete();
+        }
+
+        throw new RuntimeException("Invalid state " + state + "reached in state factory!");
+    }
+
+    Handler timerHandler = new Handler();
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            status_message.setText(state.message);
+            int delay = state_factory();
+            timerHandler.postDelayed(this, delay);
+        }
+    };
+
+    // --------------------------------------------------
+    // Activity handlers
+    // --------------------------------------------------
 
     @Override
     public final void onAccuracyChanged(Sensor sensor, int accuracy) {
@@ -90,14 +277,16 @@ public class RepActivity extends Activity implements SensorEventListener {
 
     @Override
     protected void onPause() {
-        // Be sure to unregister the sensor when the activity pauses.
+        // Be sure to unregister the sensor when the activity pauses
         super.onPause();
+        cancel_calibration();
         sensorManager.unregisterListener(this);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        cancel_calibration();
         try {
             luxDataFileStream.close();
         } catch(Exception e) {
